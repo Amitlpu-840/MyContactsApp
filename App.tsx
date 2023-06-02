325997fd5c2dd3848de3673c790b98143d915439
Amit Giri <thisisag840@gmail.com>
2023-06-02 16:43:50 +0530

    AppNavigator setup

--- App.tsx
+++ App.tsx
@@ -1,46 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
+import AppNavigator from './src/AppNavigator';
 
-import {PermissionsAndroid, Text, View} from 'react-native';
-import Contacts from 'react-native-contacts';
+const App = () => {
+  return <AppNavigator />;
+};
 
-function App(): JSX.Element {
-  //   const isDarkMode = useColorScheme() === 'dark';
-
-  //   const backgroundStyle = {
-  //     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  //   };
-
-  const [contacts, setContacts] = useState({});
-  useEffect(() => {
-    getPermission();
-  });
-
-  const getPermission = () => {
-    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
-      title: 'Contacts',
-      message: 'This app would like to view your contacts.',
-      buttonPositive: 'Please accept bare mortal',
-    }).then(res => {
-      if (res == 'granted') {
-        Contacts.getAll()
-          .then(contact => {
-            // work with contacts
-            // console.log(contacts);
-            setContacts(contact);
-          })
-          .catch(e => {
-            console.log(e);
-          });
-      }
-    });
-  };
-
-  return (
-    <>
-      <View>
-        <Text>Hello world</Text>
-      </View>
-    </>
-  );
-}
 export default App;
